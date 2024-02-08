4a69d178c7d8e6aed37066c1c0dd3a5874b9813f
Haadi Farouk <haadifarouk@gmail.com>
2024-02-08 19:10:13 +0100

    Major UI fixes and dropdowns, navbar fix for mobile view

--- assets/js/togglemenu.js
+++ assets/js/togglemenu.js
@@ -13,3 +13,41 @@ const initApp = () =>{
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+
+
+    const expandButton = document.querySelector('.expand');
+    const expandToggle =()=>{
+    const expandedContent = document.querySelector('.expanded');
+
+        
+        expandedContent.classList.toggle('hidden');
+    }
+
+    expandButton.addEventListener('click', expandToggle);
+
+
+    /* EXPAND BUTTON 2 */
+    const expandButton2 = document.querySelector('.expand2');
+    const expandToggle2 =()=>{
+    const expandedContent2 = document.querySelector('.expanded2');
+
+        
+        expandedContent2.classList.toggle('hidden');
+    }
+    
+
+    expandButton2.addEventListener('click', expandToggle2);
+
+
+    /* EXPAND BUTTON 3 */
+    const expandButton3 = document.querySelector('.expand3');
+    const expandToggle3 =()=>{
+    const expandedContent3 = document.querySelector('.expanded3');
+
+        
+        expandedContent3.classList.toggle('hidden');
+    }
+    
+
+    expandButton3.addEventListener('click', expandToggle3);
